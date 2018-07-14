14489580d1c8efbc7c9851b8762f98c2a0f02d9d
zhousheng123 <1040050116@qq.com>
2018-07-14 09:26:49 +0800

    活动主页

--- src/pages/activities/activity/index/index.vue
+++ src/pages/activities/activity/index/index.vue
@@ -1,13 +1,74 @@
 <template>
-    <d2-container type="full">
+  <d2-container type="full">
           <template slot="header">活动信息</template>
-    </d2-container>
+          <!-- <router-link :to="{ name: 'activity', params: { activityId: 123 }}"> -->
+
+<div style="margin: 20px;"></div>
+
+<el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+  <el-form-item label="活动名称">
+    <el-input v-model="formLabelAlign.name"></el-input>
+  </el-form-item>
+  <el-form-item label="活动地点">
+    <el-input v-model="formLabelAlign.region"></el-input>
+  </el-form-item>
+  <el-form-item label="活动详情">
+    <el-input v-model="formLabelAlign.type"></el-input>
+  </el-form-item>
+    <el-button type="primary">查看活动作品</el-button>
+</el-form>
+    </d2-container> 
 </template>
 
 <script>
-export default {};
-</script>
+  export default {
+    data() {
+      return {
+        labelPosition: 'right',
+        formLabelAlign: {
+          name: '',
+          region: '',
+          type: ''
+        }
+      };
+    },
+    mounted () {
+      this.onGet();
+    },
+    methods: {
+      onGet() {
+          this.$axios({
+          methods: "get",
+          url: '/xx'
+        }).then(res => {
+            this.formLabelAlign.name = res.data.name,
+            this.formLabelAlign.region=res.data.region,
+            this.formLabelAlign.type=res.data.type
+        })
+        .catch(err => {
+             this.formLabelAlign.name='111',
 
+            this.formLabelAlign.region='2',
+            this.formLabelAlign.type='3'
+            });
+        
+      }
+    }
+  }
+</script>
 <style lang="scss" scoped>
+@media (min-width: 576px) {
+  // 根据你的需要在这里添加样式
+}
+@media (min-width: 768px) {
+  // 根据你的需要在这里添加样式
+}
+@media (min-width: 992px) {
+  // 根据你的需要在这里添加样式
+}
+// 在大于1920分辨率的时候
+@media (min-width: 1921px) {
+}
 </style>
+
 
